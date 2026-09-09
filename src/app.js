9526ea43f914732386158da499ace923ff862d1c
zotel1 <zotelsigel@gmail.com>
2026-09-08 22:23:36 -0300

    agregamos middlewares y manejo de errores globales

--- src/app.js
+++ src/app.js
@@ -3,11 +3,17 @@ import cors from 'cors'
 
 import healthRoutes from './routes/health.routes.js'
 
+import { notFound } from './middlewares/notFound.middleware.js'
+import { errorHandler } from './middlewares/error.middleware.js'
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/health', healthRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 export default app
