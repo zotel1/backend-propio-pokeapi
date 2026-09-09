5e9198c9cf5ba3923ca9bdcb1c498890316e758f
zotel1 <zotelsigel@gmail.com>
2026-09-08 23:24:11 -0300

    adaptamos cors

--- src/app.js
+++ src/app.js
@@ -1,17 +1,37 @@
 import express from 'express'
 import cors from 'cors'
-import typeRoutes from './routes/type.routes.js'
-import collectionRoutes from './routes/collection.routes.js'
 
 import healthRoutes from './routes/health.routes.js'
 import pokemonRoutes from './routes/pokemon.routes.js'
+import typeRoutes from './routes/type.routes.js'
+import collectionRoutes from './routes/collection.routes.js'
 
 import { notFound } from './middlewares/notFound.middleware.js'
 import { errorHandler } from './middlewares/error.middleware.js'
 
 const app = express()
 
-app.use(cors())
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.FRONTEND_PRODUCTION_URL
+].filter(Boolean)
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+        return
+      }
+
+      const error = new Error('Origen no permitido por CORS')
+      error.statusCode = 403
+
+      callback(error)
+    }
+  })
+)
+
 app.use(express.json())
 
 app.use('/api/health', healthRoutes)
